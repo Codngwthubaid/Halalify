f54cdfee2be4275af9c306cc4ab7262bf4c8d497
Codngwthubaid <codngwthubaid@gmail.com>
2025-04-13 12:39:19 +0530

    design all the fundamental concept for backend logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,7 @@ export default function App() {
       <SignedIn>
         <UserButton />
       </SignedIn>
+      <div className="text-2xl text-orange-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi sit et facere deleniti sint, nam ab nemo rem voluptatibus necessitatibus est odio veritatis culpa dolorum delectus cumque vel libero! Illo.</div>
     </main>
   )
 }
