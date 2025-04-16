95470f87251473c5dbb244d8393d35da7b025a94
Codngwthubaid <codngwthubaid@gmail.com>
2025-04-16 09:43:16 +0530

    init UI presented songs functions and also correct the album controller file name and also set the gitignore file

--- backend/src/routes/songs.route.js
+++ backend/src/routes/songs.route.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { protectedRoute, requireAdmin } from "../middleware/auth.middleware.js";
+import { getAllSongs, getFeaturedSongs, getMadeForYouSongs, getTrendingSongs } from "../controller/song.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("songs");
-});
+router.get("/", protectedRoute, requireAdmin, getAllSongs);
+router.get("/featuredSongs", getFeaturedSongs);
+router.get("/made-for-you", getMadeForYouSongs);
+router.get("/trending", getTrendingSongs);
 
 export default router;
