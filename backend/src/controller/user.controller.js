fba3a316f3e78cff7e43f2844686ee8d6cadff63
Codngwthubaid <codngwthubaid@gmail.com>
2025-05-10 08:11:32 +0530

    create the router and the controller for the message and completes the basic messaging sockets in the backend

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -1,12 +1,28 @@
 import { User } from "../models/user.model.js"
+import { Message } from "../models/message.model.js"
 
 export const getAllUsers = async (req, res, next) => {
     try {
         const currentUserId = req.auth.userId
-        const users = await User.find({clerkId : {$ne: currentUserId}})
+        const users = await User.find({ clerkId: { $ne: currentUserId } })
         res.status(200).json({ success: true, message: "Users fetched successfully except current user", users })
     } catch (error) {
         console.log("Error present in get all users route", error.message)
         next(error)
     }
+}
+
+export const getMessages = async (req, res, next) => {
+    try {
+        const myId = req.auth.userId
+        const { userID } = req.params
+        console.log(myId, userID)
+
+        const message = await Message.find({ $or: [{ senderId: myId, receiverId: userID }, { senderId: userID, receiverId: myId }] }).sort({ createdAt: 1 })
+        res.status(200).json({ success: true, message: "Messages fetched successfully", message })
+    } catch (error) {
+        console.log("Error present in get messages route", error.message)
+        next(error)
+    }
+
 }
--- backend/src/lib/socket_io.js
+++ backend/src/lib/socket_io.js
@@ -35,7 +35,7 @@ export const initSocketServer = (httpServer) => {
         })
 
         // ya socket batayga ki konse user ne kisko message dala hain
-        socket.ion("send_message", async (data) => {
+        socket.on("send_message", async (data) => {
             try {
                 const { senderId, receiverId, message } = data
                 // database main baatain store hongi
@@ -51,6 +51,21 @@ export const initSocketServer = (httpServer) => {
                 console.log(" Error present in send message:", error)
                 socket.emit("message_error", error.message)
             }
+        })
+
+        // ya socket user ko disconnect karega and ausski chats and activity ko bhi delete krdega
+        socket.on("disconnected", () => {
+            let disConnnectedUserId;
+            for (const [userId, socketId] of userSockets.entries()) {
+                if (socketId === socket.id)
+                    disConnnectedUserId = userId
+                userSockets.delete(userId)
+                userActivity.delete(userId)
+                break
+            }
+
+            // ya batayga baki sab users ko ki ab ya disconncted ho chuka hain  
+            if (disConnnectedUserId) io.emit("user_disconnected", disConnnectedUserId)
         })
 
     })
