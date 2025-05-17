c3b1a2b06215168435825a88be6141a93987eff3
Codngwthubaid <codngwthubaid@gmail.com>
2025-05-17 15:09:13 +0530

    change the frontend setting and rebuild the frontend dist and reinstall the root package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,31 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Suspense, lazy } from "react";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 import { AuthCallBackPage } from "./pages/auth-callback/AuthCallBackPage";
-
-
-// Lazy-loaded components
-const HomePage = lazy(() => import("./pages/home/HomePage"));
-const MainLayout = lazy(() => import("./Layout/MainLayout"));
-const AlbumPage = lazy(() => import("./pages/album/AlbumPage"));
-const AdminPage = lazy(() => import("./pages/admin/AdminPage"));
+import HomePage from "./pages/home/HomePage";
+import MainLayout from "./Layout/MainLayout";
+import AlbumPage from "./pages/album/AlbumPage";
+import AdminPage from "./pages/admin/AdminPage";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route
-            path="/sso-callback"
-            element={
-              <AuthenticateWithRedirectCallback
-                signInFallbackRedirectUrl={"/auth-callback"}
-              />
-            }
-          />
-          <Route path="/auth-callback" element={<AuthCallBackPage />} />
-          <Route path="/admin" element={<AdminPage />} />
+      <Routes>
+        <Route
+          path="/sso-callback"
+          element={
+            <AuthenticateWithRedirectCallback
+              signInFallbackRedirectUrl={"/auth-callback"}
+            />
+          }
+        />
+        <Route path="/auth-callback" element={<AuthCallBackPage />} />
+        <Route path="/admin" element={<AdminPage />} />
 
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/albums/:albumId" element={<AlbumPage />} />
-          </Route>
-        </Routes>
-      </Suspense>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/albums/:albumId" element={<AlbumPage />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
