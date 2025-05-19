155fba6e8c473835bf0d5900051df55451ff37c6
Codngwthubaid <codngwthubaid@gmail.com>
2025-05-19 10:14:26 +0530

    add skeleton on made for you and trending songs

--- frontend/src/components/skeletons/gridSelectionLayoutSkeletonForSongs.tsx
+++ frontend/src/components/skeletons/gridSelectionLayoutSkeletonForSongs.tsx
@@ -1,5 +1,20 @@
+import { Skeleton } from "../ui/skeleton";
+
 export default function GridSelectionLayoutSkeletonForSongs() {
-    return(
-        <div>skeleton</div>
+    return (
+        <div className="flex flex-wrap gap-4 justify-evenly items-center my-5">
+            {Array.from({ length: 4 }).map((_, index) => (
+                <div
+                    key={index}
+                    className="flex flex-col gap-2 justify-start items-center "
+                >
+                    <Skeleton className="h-[150px] w-[150px] rounded-xl" />
+                    <div className="space-y-2">
+                        <Skeleton className="h-2 w-[130px]" />
+                        <Skeleton className="h-2 w-[100px]" />
+                    </div>
+                </div>
+            ))}
+        </div>
     )
 }
